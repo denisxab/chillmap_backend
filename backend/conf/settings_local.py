ec3eb42bd6114fa4c55faedd0c8726d72d98ef12
Your Name <you@example.com>
2023-05-12 20:56:55 +0300

    "Пт 12 мая 2023 20:56:55" -

--- backend/conf/settings_local.py
+++ backend/conf/settings_local.py
@@ -26,7 +26,7 @@ ALLOWED_HOSTS_SELECT = ["*"]
 #
 SECRET_KEY_SELECT = "django-insecure-ah3y)b6&1l_&$#v4k8%oui3c!5l=81985j5k6lp&!17k^2raur"
 #
-DEBUG_SELECT = True
+DEBUG_SELECT = True if os.environ["DEBUG"] == "True" else False
 #
 IP_ADR = os.environ.get("IP_ADR", "localhost")
 # Хост для раздачи сатических файлов
