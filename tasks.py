fdf804ee2c00848076d0b8162861d4aea1bbc96c
Your Name <you@example.com>
2023-05-20 00:39:41 +0300

    "Сб 20 мая 2023 00:39:41" -

--- tasks.py
+++ tasks.py
@@ -80,11 +80,9 @@ def deletePgdata(ctx, limit):
 @task
 def testDjango(ctx, prod=False):
     """Выполнить тесты Django в контейнере"""
-    print("Запуск 1::::::::::::;;")
+    print("::::::::::::Запуск docker-compose::::::::::::")
     restart(ctx, prod=prod, detach=True, list_prod_app=LIST_TEST_DJANGO_APP)
-    print("Начало 2::::::::::::;;")
-    time.sleep(5)
-    print("Запуск 2::::::::::::;;")
+    print("::::::::::::Выполнение теста::::::::::::")
     command = "pytest"
     ctx.run(f"docker-compose exec app {command}", pty=True)
 
